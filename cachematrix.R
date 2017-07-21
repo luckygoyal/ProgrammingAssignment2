0ec0a04b7a6fe8b15893d8f164ea596152c92410
luckygoyal <m.goyal19@gmail.com>
2017-07-21 19:20:58 +0530

    Code added in R file
    modified R script after making the changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
-## functions do
+##name: Mayank Goyal
+## This function will compute the inverse matrix using cache
+## This function will create an image in a different environment using the <<- operator 
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+       z <- NULL
+       Set <- function(y) {
+            x <<- y
+            z <<- NULL
+        }
+        Get <- function() x
+        SetInverse <- function(mean) z <<- mean
+        GetInverse <- function() z
+        list(Set = Set, SetInverse = SetInverse, Get = Get, GetInverse = GetInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of a matrix using solve function
 
 cacheSolve <- function(x, ...) {
+       z <- x$GetInverse()
+        if(!is.null(z)) {
+                message("getting cached data")
+                return(z)
+        }
+        data <- x$Get()
+        z <- solve(data, ...)
+        x$SetInverse(z)
+        z
         ## Return a matrix that is the inverse of 'x'
 }
